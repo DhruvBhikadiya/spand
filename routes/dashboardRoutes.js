255c56777e60bcfc5f8f736e26bd45858013fc1c
Dhruv Bhikadiya <138368408+DhruvBhikadiya@users.noreply.github.com>
2025-03-25 12:56:32 +0530

    Delete routes directory

--- routes/dashboardRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const DashboardController = require('../controllers/dashboardController');
-
-router.get('/superAdminDashboard', DashboardController.superadminDashboard);
-
-module.exports = router;
